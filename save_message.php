a6186cb5523534246ca30d36bb4a3876fb8256fb
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-04-21 16:41:33 +0530

    added updarted files

---
+++ save_message.php
@@ -0,0 +1,23 @@
+<?php
+// Connect to your database
+$host = 'localhost';
+$dbname = 'websocket_demo';
+$user = 'root';
+$password = '';
+$conn = new PDO("mysql:host=$host;dbname=$dbname", $user, $password);
+
+// Check if the request is a POST request
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $sender = $_POST['sender'];
+    $message = $_POST['message'];
+
+    // Save message to database
+    $sql = "INSERT INTO chat_messages (sender, message) VALUES (:sender, :message)";
+    $stmt = $conn->prepare($sql);
+    $stmt->bindParam(':sender', $sender);
+    $stmt->bindParam(':message', $message);
+    $stmt->execute();
+
+    echo "Message sent";
+}
+?>
--- server.php
+++ server.php
@@ -8,41 +8,28 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
 class Chat implements MessageComponentInterface {
-    protected $clients;
-    protected $db;
-
-    public function __construct() {
-        $this->clients = new \SplObjectStorage;
-        $this->db = new mysqli('localhost', 'root', '', 'websocket_demo');
-    }
-
     public function onOpen(ConnectionInterface $conn) {
-        $this->clients->attach($conn);
+        echo "New connection: {$conn->resourceId}\n";
     }
 
     public function onMessage(ConnectionInterface $from, $msg) {
-        if (strpos($msg, ": ") !== false) {
-            list($sender, $message) = explode(": ", $msg, 2);
-            $stmt = $this->db->prepare("INSERT INTO chat_messages (sender, message) VALUES (?, ?)");
-            if ($stmt) {
-                $stmt->bind_param("ss", $sender, $message);
-                $stmt->execute();
-                $stmt->close();
-            }
+        foreach ($from->httpRequest->getHeaders() as $header => $values) {
+            echo "$header: " . implode(', ', $values) . "\n";
         }
-
-        foreach ($this->clients as $client) {
-            if ($from !== $client) {
-                $client->send($msg);
-            }
-        }
+        echo "Message: $msg\n";
+        // foreach ($from->getAllConnections() as $client) {
+        //     if ($client !== $from) {
+        //         $client->send($msg);
+        //     }
+        // }
     }
 
     public function onClose(ConnectionInterface $conn) {
-        $this->clients->detach($conn);
+        echo "Connection {$conn->resourceId} has disconnected\n";
     }
 
     public function onError(ConnectionInterface $conn, \Exception $e) {
+        echo "Error: {$e->getMessage()}\n";
         $conn->close();
     }
 }
@@ -53,8 +40,9 @@ $server = IoServer::factory(
             new Chat()
         )
     ),
-    8888,
-    '0.0.0.0'
+    8888
 );
 
 $server->run();
+
+?>

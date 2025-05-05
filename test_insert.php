d84458050a4119a36bbd03f8cca4c7a640ade888
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-05-05 12:44:52 +0530

    Update test_insert.php

--- test_insert.php
+++ test_insert.php
@@ -3,10 +3,8 @@ $conn = new mysqli('localhost', 'root', '', 'websocket_demo');
 if ($conn->connect_error) {
     die("DB Error: " . $conn->connect_error);
 }
-
 $stmt = $conn->prepare("INSERT INTO chat_messages (sender, message) VALUES (?, ?)");
 $sender = "TestUser";
 $message = "Hello world";
 $stmt->bind_param("ss", $sender, $message);
 $stmt->execute();
-

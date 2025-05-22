5949ec5345fda4898ff31f340fef431a27b818b8
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-05-22 21:51:14 +0530

    Update test_insert.php

--- test_insert.php
+++ test_insert.php
@@ -4,7 +4,9 @@ if ($conn->connect_error) {
     die("DB Error: " . $conn->connect_error);
 }
 $stmt = $conn->prepare("INSERT INTO chat_messages (sender, message) VALUES (?, ?)");
+
 $sender = "TestUser";
 $message = "Hello world";
+
 $stmt->bind_param("ss", $sender, $message);
 $stmt->execute();

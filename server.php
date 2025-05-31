ea00eb18fb5c44493d6d0f2fa6fcc2306cb27f76
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-05-31 22:13:49 +0530

    added multiple users logout login option

--- server.php
+++ server.php
@@ -8,28 +8,38 @@ use Ratchet\Http\HttpServer;
 use Ratchet\WebSocket\WsServer;
 
 class Chat implements MessageComponentInterface {
+    protected $clients;
+
+    public function __construct() {
+        $this->clients = new \SplObjectStorage;
+    }
+
     public function onOpen(ConnectionInterface $conn) {
+        $this->clients->attach($conn);
         echo "New connection: {$conn->resourceId}\n";
     }
+
     public function onMessage(ConnectionInterface $from, $msg) {
-        foreach ($from->httpRequest->getHeaders() as $header => $values) {
-            echo "$header: " . implode(', ', $values) . "\n";
-        }
         echo "Message: $msg\n";
-        // foreach ($from->getAllConnections() as $client) {
-        //     if ($client !== $from) {
-        //         $client->send($msg);
-        //     }
-        // }
+        foreach ($this->clients as $client) {
+            // Send to everyone (including sender)
+            $client->send($msg);
+        }
     }
+
     public function onClose(ConnectionInterface $conn) {
+        $this->clients->detach($conn);
         echo "Connection {$conn->resourceId} has disconnected\n";
     }
+
     public function onError(ConnectionInterface $conn, \Exception $e) {
         echo "Error: {$e->getMessage()}\n";
         $conn->close();
+        $this->clients->detach($conn);
     }
 }
+
+
 $server = IoServer::factory(
     new HttpServer(
         new WsServer(
@@ -38,5 +48,7 @@ $server = IoServer::factory(
     ),
     8888
 );
+
 $server->run();
+
 ?>

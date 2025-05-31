c0f4fbc0df3f844513d26a0396e394b85fbf6bee
Prathamesh Patil <94956959+Prath7057@users.noreply.github.com>
2025-05-31 22:14:10 +0530

    added multiple users logout login option

--- get_messages.php
+++ get_messages.php
@@ -2,16 +2,14 @@
 header('Content-Type: application/json');
 
 $conn = new mysqli('localhost', 'root', '', 'websocket_demo');
-
 if ($conn->connect_error) {
     die(json_encode(['error' => $conn->connect_error]));
 }
 
-$sql = "SELECT sender, message, created_at FROM chat_messages ORDER BY id DESC LIMIT 30";
+$sql = "SELECT sender, message, created_at FROM chat_messages_new ORDER BY id DESC LIMIT 30";
 $result = $conn->query($sql);
 
 $messages = [];
-
 while ($row = $result->fetch_assoc()) {
     $messages[] = $row;
 }
